ae1dca35b65d518a281c6b34b0b9fe164e7d4f2b
moh4b-z <mohammadsalim10000@gmail.com>
2024-12-12 23:54:45 -0300

    Ajustes no teste finalizados

--- module/functions.js
+++ module/functions.js
@@ -7,7 +7,7 @@ const urlraiz = 'https://api-estudos.onrender.com'
 const urlHTML = 'https://api-estudos.onrender.com/static'
 
 function teste(){
-    objectReturn = {file: `${urlHTML}SENAI-2024-2025/2- Front-end 2° semestre/7- layout_figma-bb/index.html`}
+    objectReturn = {file: `${urlHTML}/SENAI-2024-2025/2- Front-end 2° semestre/7- layout_figma-bb/index.html`}
     return objectReturn
 }
 
