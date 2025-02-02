77d8a4b4c255ee579d5622bcad2b0f921c14f1b5
moh4b-z <mohammadsalim10000@gmail.com>
2025-02-02 00:06:14 -0300

    Remoção dos  arquivos antigos de class

--- src/utils/pathUtils.js
+++ src/utils/pathUtils.js
@@ -1,4 +1,36 @@
+const Folders = require('../services/FolderService')
+const Files = require('../services/FileService')
+
+const urlraiz = 'https://api-estudos.onrender.com'
 const urlHTML = 'https://api-estudos.onrender.com/static/'
+
+
+function teste(){
+    objectReturn = {file: `${urlHTML}SENAI-2024-2025/2- Front-end 2° semestre/7- layout_figma-bb/index.html`}
+    return objectReturn
+}
+
+function searchWith(Path, File, Folder, Exception, Name){
+    let path = Path ? Path : './study-material/'
+    let root = new Folders(path)
+    let file = File
+    let folder =  Folder
+    let exception = Exception
+    let name = Name
+    let objectReturn = false
+    if(file && folder){
+        objectReturn = root.searchAllWith(name, exception)
+    }else if(file){
+        objectReturn = root.searchAllFilesWith(name, exception)
+    }else if(folder){
+        objectReturn = root.searchAllFolderWith(name, exception)
+    }else{
+        objectReturn = false
+    }
+    
+
+    return objectReturn
+}
 
 function pathTransformedLink(path) {
     let link = path.replace('../study-material/', urlHTML)
@@ -7,5 +39,6 @@ function pathTransformedLink(path) {
 }
 
 module.exports = {
+    searchWith,
     pathTransformedLink
 }
