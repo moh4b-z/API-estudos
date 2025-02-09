3377ee5c271740c620528c60d02753d400014b1f
moh4b-z <mohammadsalim10000@gmail.com>
2025-02-09 13:48:04 -0300

    Tentativa de correção de caminho

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 const path = require('path')
-app.use('/static', express.static(path.resolve(__dirname, '../study-material')))
+app.use('/static', express.static(path.resolve(__dirname, './study-material')))
 
 
 
